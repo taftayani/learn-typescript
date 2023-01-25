a8769c861bedc291f2f06dc929399dcf9fda78df
Muhammad Breda Taftayani <Muhammad_B782@mitrais.com>
2023-01-25 15:28:32 +0700

    #update

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,15 @@
+import { Inter } from '@next/font/google';
+import Link from 'next/link';
+import Header from '@/Component/Header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<>
+			<Header title={'Home'} />
+			<Link href={'/'}>Go back Home</Link>
+			<h1>List of Details</h1>
+		</>
+	);
+}
