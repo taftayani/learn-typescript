e93a44ff6299b7366fd520cca2135a3a3cb0effc
taftayani <taftayani123@gmail.com>
2023-03-13 11:14:12 +0700

    #update redux management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import { store } from '@/store';
+import { persistor, store } from '@/store';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
-			<Component {...pageProps} />
+			<PersistGate persistor={persistor}>
+				<Component {...pageProps} />
+			</PersistGate>
 			{''}
 		</Provider>
 	);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import Header from '../component/Header';
 import Link from 'next/link';
-import { Typography, Input } from 'antd';
-
-const inter = Inter({ subsets: ['latin'] });
-
+import { Typography, Input, Button } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '@/store/counter/counterSlice';
+import { RootState } from '@/store';
 export default function Home() {
+	const dispatch = useDispatch();
+	const count = useSelector((state: RootState) => state.counter.value);
 	return (
 		<>
 			<Header title={'Home'} />
 			<Typography.Title className="!text-6xl text-right !text-yellow-300">TES</Typography.Title>
 			<Input placeholder="please input text"></Input>
+			<Button onClick={() => dispatch(increment())}>Tambah</Button>
+			<Typography.Text className="">{count}</Typography.Text>
+			<Button onClick={() => dispatch(decrement())}>kurang</Button>
 			<main className={styles.main}>
 				<Link href={'/detail'}>Go to details</Link>
 			</main>
